a2d0e0cf5a680a81ebc8e98743efd424570b7d2d
oldwang12 <806459794@qq.com>
2024-02-27 11:09:25 +0800

    2024/02/27 11:09:25

--- common/common.go
+++ common/common.go
@@ -4,8 +4,11 @@ import (
 	"crypto/md5"
 	"fmt"
 	"strings"
+	"time"
 
 	"mvdan.cc/xurls/v2"
+
+	"github.com/avast/retry-go"
 )
 
 func MD5(s string) string {
@@ -33,12 +36,28 @@ func GetLinks(text string) []string {
 	return xurls.Strict().FindAllString(text, -1)
 }
 
+// s: https://www.baidu.com/query
+// seq: /
+// 返回: query
 func GetLastIndexValue(s, sep string) string {
 	strs := strings.Split(s, sep)
 	return strs[len(strs)-1]
 }
 
+// s: https://www.baidu.com/query
+// seq: /
+// 返回: https://www.baidu.com/
 func GetBeforeLastIndexValue(s, sep string) string {
 	lastIndexValue := GetLastIndexValue(s, sep)
 	return strings.TrimRight(s, lastIndexValue)
+}
+
+// 多次执行函数
+func RetryFunc(fn func() error, retryAttempts int, waitTime time.Duration) error {
+	return retry.Do(
+		fn,
+		retry.Delay(waitTime),
+		retry.Attempts(uint(retryAttempts)),
+		retry.DelayType(retry.FixedDelay),
+	)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/avast/retry-go v3.0.0+incompatible
 	k8s.io/klog/v2 v2.120.1
 	mvdan.cc/xurls/v2 v2.5.0
 )
@@ -11,5 +12,6 @@ require (
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/net v0.7.0 // indirect
 )
