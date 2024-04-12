8c4494333936efc116fe6b6554230a712f1539c3
oldwang12 <806459794@qq.com>
2024-04-12 15:40:49 +0800

    2024/04/12 15:40:49

---
+++ common/ssh.go
@@ -0,0 +1,47 @@
+package common
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+)
+
+// 记得 defer client.Close()
+func ClientSSH(command, serverAddr, sshPort, sshUser, privateKeyPath string) (*ssh.Client, error) {
+	// 读取私钥文件
+	key, err := os.ReadFile(privateKeyPath)
+	if err != nil {
+		return nil, fmt.Errorf("读取私钥文件失败:%v", err)
+	}
+
+	// 使用私钥创建认证对象
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		return nil, fmt.Errorf("使用私钥创建认证对象:%v", err)
+	}
+
+	// 配置SSH客户端
+	config := &ssh.ClientConfig{
+		User: sshUser,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // 忽略主机密钥检查（不建议在生产环境中使用）
+	}
+
+	// 建立SSH连接
+	return ssh.Dial("tcp", serverAddr+":"+sshPort, config)
+}
+
+func ExecSSHCommand(client *ssh.Client, command string) (string, error) {
+	// 执行远程命令
+	session, err := client.NewSession()
+	if err != nil {
+		return "", fmt.Errorf("无法创建SSH会话: %v", err)
+	}
+	defer session.Close()
+	// 运行命令
+	output, err := session.CombinedOutput(command)
+	return string(output), err
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.21
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.1
-	github.com/avast/retry-go v3.0.0+incompatible
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	k8s.io/klog/v2 v2.120.1
 	mvdan.cc/xurls/v2 v2.5.0
 )
@@ -12,6 +12,6 @@ require (
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 )
